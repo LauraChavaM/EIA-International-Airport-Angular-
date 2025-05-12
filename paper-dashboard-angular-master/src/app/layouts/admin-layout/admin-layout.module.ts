3baf1bc21dea5baa8e6cca30091155af39ffefbd
laura chavarriaga <laurachaman@icloud.com>
2025-05-12 11:41:11 -0500

    set folders

--- paper-dashboard-angular-master/src/app/layouts/admin-layout/admin-layout.module.ts
+++ paper-dashboard-angular-master/src/app/layouts/admin-layout/admin-layout.module.ts
@@ -7,7 +7,7 @@ import { AdminLayoutRoutes } from './admin-layout.routing';
 
 import { DashboardComponent }       from '../../pages/dashboard/dashboard.component';
 import { UserComponent }            from '../../pages/user/user.component';
-import { TableComponent }           from '../../pages/table/table.component';
+import { TableComponent }           from '../../pages/flights/table.component';
 import { TypographyComponent }      from '../../pages/typography/typography.component';
 import { IconsComponent }           from '../../pages/icons/icons.component';
 import { MapsComponent }            from '../../pages/maps/maps.component';
